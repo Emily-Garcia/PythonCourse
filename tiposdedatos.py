493aa127977c35c7dc74fbef45f70e56f7ae6843
Emily-Garcia <52437193+Emily-Garcia@users.noreply.github.com>
2020-04-02 12:01:05 -0600

    Creamos un programa para sumar y multiplicar dos numeros

---
+++ tiposdedatos.py
@@ -0,0 +1,40 @@
+nombre = "Emily"
+
+print("Hola, " + nombre)
+
+#TIPOS DE DATOS
+entero = 15
+flotantes = 5.3423
+string = "Texto"
+booleano = True
+
+# + - * ** / // %
+#OPERADORES ARITMETICOS
+suma = 4 + 5
+resta = 5 - 4
+multiplicacion = 5 * 4
+potencia = 5 ** 2
+division = 10 / 2
+division_exacta = 5 // 2
+modulo = 10 % 2
+
+# print("El valor de tu suma es:", suma)
+
+# + es para concatenar solo texto
+
+#Para pedirle datos al usuario
+print("Ingresa dos numeros a sumar")
+numero_1 = float(input("Ingresa el primer valor: "))
+numero_2 = float(input("Ingresa el segundo valor: "))
+
+total = numero_1 + numero_2
+
+print("Total de la suma es:","{:0.2f}".format(total))
+
+print("Ingresa dos numeros a multiplicar")
+numero_1 = float(input("Ingresa el primer valor: "))
+numero_2 = float(input("Ingresa el segundo valor: "))
+
+total = numero_1 * numero_2
+
+print("Total de la multipicacion es:", "{:0.3f}".format(total))
