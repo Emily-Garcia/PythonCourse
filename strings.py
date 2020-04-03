2aeef80eaad173b07c584715063f0c7f54f25134
Emily-Garcia <52437193+Emily-Garcia@users.noreply.github.com>
2020-04-03 12:07:20 -0600

    Acabamos de aprender Strings

--- strings.py
+++ strings.py
@@ -34,7 +34,39 @@ print(slicito_2)
 print(slicito_3)
 
 print("-----------")
+#Separar strings
 splited = mensaje.split(" ")
 print(splited)
 splited = mensaje.split("a")
 print(splited)
+print(splited[1])
+
+print("---------")
+#LISTAS
+lista = ["palabra", 3, 5.43, True, ["soy","otra","lista"]]
+#meternos a uns lista anidada
+print(lista[4][1])
+#lista_anidada = lista[4]
+#lista_anidada[1]
+
+print("-------")
+#Elemento quitado
+nueva_lista = lista.pop()
+#Quita el ultimo elemento de la lista
+lista.pop()
+print(lista)
+print(nueva_lista)
+
+print("------------")
+#Agregar elemento al final
+lista.append(False)
+print(lista)
+
+print("----------------")
+#JOIN
+#toma un lista de strings forzosamente y lo convierte a un solo string separado por el separador que le pongas
+lista_string = ["1","2","3","4"]
+separador = "-"
+
+lista_joineada = separador.join(lista_string)
+print(lista_joineada)
